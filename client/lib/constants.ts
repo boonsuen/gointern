1f2238bdbd3f6f5e34a5b6add123e795a562abc3
Boonsuen Oh <19908657+boonsuen@users.noreply.github.com>
2023-09-06 00:15:23 +0800

    Update to API Gateway endpoint

--- client/lib/constants.ts
+++ client/lib/constants.ts
@@ -1,4 +1,4 @@
 export const API_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:4000/api'
-    : 'http://54.158.50.41/api';
+    : 'https://53lhr13ezc.execute-api.us-east-1.amazonaws.com/api';
