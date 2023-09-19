e3ce4c2f04057462d03d4b58e5955919b3653927
Boonsuen Oh <19908657+boonsuen@users.noreply.github.com>
2023-09-20 01:08:04 +0800

    Update server.py

--- server/server.py
+++ server/server.py
@@ -30,61 +30,9 @@ app.register_blueprint(companies, url_prefix="/api/companies")
 app.register_blueprint(admins, url_prefix="/api/admins")
 
 
-@app.route("/api/testing", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return jsonify({"message": "Testing", "success": True})
-
-
-# @app.route("/addemp", methods=["POST"])
-# def AddEmp():
-#     emp_id = request.form["emp_id"]
-#     first_name = request.form["first_name"]
-#     last_name = request.form["last_name"]
-#     pri_skill = request.form["pri_skill"]
-#     location = request.form["location"]
-#     emp_image_file = request.files["emp_image_file"]
-
-#     insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
-#     cursor = db_conn.cursor()
-
-#     if emp_image_file.filename == "":
-#         return "Please select a file"
-
-#     try:
-#         cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location))
-#         db_conn.commit()
-#         emp_name = "" + first_name + " " + last_name
-#         # Uplaod image file in S3 #
-#         emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
-#         s3 = boto3.resource("s3")
-
-#         try:
-#             print("Data inserted in MySQL RDS... uploading image to S3...")
-#             s3.Bucket(custombucket).put_object(
-#                 Key=emp_image_file_name_in_s3, Body=emp_image_file
-#             )
-#             bucket_location = boto3.client("s3").get_bucket_location(
-#                 Bucket=custombucket
-#             )
-#             s3_location = bucket_location["LocationConstraint"]
-
-#             if s3_location is None:
-#                 s3_location = ""
-#             else:
-#                 s3_location = "-" + s3_location
-
-#             object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
-#                 s3_location, custombucket, emp_image_file_name_in_s3
-#             )
-
-#         except Exception as e:
-#             return str(e)
-
-#     finally:
-#         cursor.close()
-
-#     print("all modification done...")
-#     return render_template("AddEmpOutput.html", name=emp_name)
+    return jsonify({"message": "The API server is running!", "success": True})
 
 
 if __name__ == "__main__":
